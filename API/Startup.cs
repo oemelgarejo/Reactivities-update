54d45cc0613f62a4f412325deb35a6d0abd95222
Oscar Melgarejo <oemelgarejo3473@gmail.com>
2021-08-26 15:43:24 -0500

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using MediatR;
+using Application.Activities;
+using AutoMapper;
+using Application.core;
+using API.Extensions;
 
 namespace API
 {
@@ -22,26 +27,13 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt => {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"); 
-                });
-            });
+            services.AddAplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,7 +51,6 @@ namespace API
             app.UseRouting();
             app.UseCors("CorsPolicy");
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
